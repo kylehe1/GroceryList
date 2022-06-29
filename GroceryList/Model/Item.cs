ef1301192fd7006a723d04e0e98177daebd38891
kylehe1 <kylehe1@gmail.com>
2022-06-29 16:38:41 -0700

    Add project files.

---
+++ GroceryList/Model/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryList.Model
+{
+    internal class Item
+    {
+    }
+}
